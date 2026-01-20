6902d4a3de3e49efeea07ad6bdde738c3d3aaec9
work <137403706+AltheaVestrit@users.noreply.github.com>
2026-01-20 16:02:59 +0100

    Refactor: In RotatingShape, make rotateRight the base function, and let rotateLeft call rotateRight

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -20,20 +20,20 @@ export class RotatingShape {
         return str;
     }
 
-    rotateLeft() {
+    rotateRight() {
         let rows = this.#shape.length;
         let cols = this.#shape[0].length;
         let rotated = new Array(cols);
         for (let i=0; i<cols; i++) {
             rotated[i] = new Array(rows);
             for (let j=0; j<rows; j++) {
-                rotated[i][j] = this.#shape[j][rows-1-i];
+                rotated[i][j] = this.#shape[cols-1-j][i];
             }
         }
         return new RotatingShape(rotated);
     }
 
-    rotateRight() {
-        return this.rotateLeft().rotateLeft().rotateLeft();
+    rotateLeft() {
+        return this.rotateRight().rotateRight().rotateRight();
     }
 }
