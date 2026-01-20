01536214debd522f3b6375fc5c4b1c4b6cf5d42d
work <137403706+AltheaVestrit@users.noreply.github.com>
2026-01-20 11:20:52 +0100

    Create fromString method for RotatingShape class

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,3 +1,17 @@
 export class RotatingShape {
-    
+    #shape;
+
+    static fromString(shape) {
+        return new RotatingShape(
+            shape
+                .replaceAll(" ","")
+                .trim()
+                .split('\n')
+                .map((row) => row.split(''))
+        )
+    }
+
+    constructor(shape) {
+        this.#shape = shape;
+    }
 }
