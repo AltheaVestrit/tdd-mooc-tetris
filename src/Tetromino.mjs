b05989b3f5ce2ac7b7d3a513176958658312e1c6
work <137403706+AltheaVestrit@users.noreply.github.com>
2026-01-20 16:10:49 +0100

    WIP: change Tetromino > constructor to handle orientations

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -20,15 +20,17 @@ export class Tetromino {
 
     static fromString(shapeType, nrOrientations) {
         const shape = RotatingShape.fromString(shapeType);
-        let orientations = [];
-        for (let i = 0; i < nrOrientations; i++) {
-            orientations.push()
-        }
-        return new Tetromino(shape);
+        let orientations = [shape, 
+            shape.rotateRight(), 
+            shape.rotateRight().rotateRight(), 
+            shape.rotateRight().rotateRight().rotateRight()
+        ].slice(0,nrOrientations);
+        return new Tetromino(shape, orientations);
     }
 
-    constructor(shape) {
+    constructor(shape, orientations) {
         this.#shape = shape;
+        this.#orientations = orientations;
     }
     toString() {
         return this.#shape.toString();
