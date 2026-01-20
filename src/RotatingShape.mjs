bca8e0c3ca60f7c4efadd5b8536ac3da46b211f7
work <137403706+AltheaVestrit@users.noreply.github.com>
2026-01-20 11:40:06 +0100

    WIP: rotateRight method in RotatingShape

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -16,7 +16,14 @@ export class RotatingShape {
     }
 
     toString() {
-    let str = this.#shape.map(row => row.join('')).join('\n') + '\n';
-    return str;
+        let str = this.#shape.map(row => row.join('')).join('\n') + '\n';
+        return str;
+    }
+
+    rotateRight() {
+        let rows = this.#shape.length;
+        let cols = this.#shape[0].length;
+        let rotated = new Array(rows);
+        rotated.fill("").map((row) => {let row_arr = new Array(cols); return row_arr.fill("");});
     }
 }
